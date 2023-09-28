0beae85b9f5f6061da2ba189d0bf96a1b5d9103c
jhon65496 <jhon65496@yandex.ru>
2023-09-28 13:07:02 +0300

    Добавьте файлы проекта.

---
+++ LogerWinForm/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogerWinForm
+{
+    public partial class Form1 : Form
+    {
+        Loger loger;
+        public Form1()
+        {
+            InitializeComponent();
+            loger = new Loger();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Log();
+        }
+
+
+        public void Log()
+        {
+            loger.Log(" ");
+            loger.Log("=== === === === === === === === === === === === === === ===");
+            loger.Log("Класс Program. Main() === === === === === === === === === ===");
+        }
+    }
+}
---
+++ LogerWinForm/Loger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace LogerWinForm
+{
+    public class Loger
+    {
+        string path = "LogAppLogerWinForm.txt";
+
+        public void Log(string message)
+        {
+            string messageFull = DateTime.Now.ToString("yyyy.MM.dd || hh:mm:ss:ffff" + " || " + message); //++ || 08:46:22:7034            
+            WriteLine(messageFull);
+        }
+        
+        public void WriteLine(string messageFull)
+        {
+            using (StreamWriter logger = new StreamWriter(path, true, Encoding.GetEncoding("UTF-8")))
+            {
+                logger.WriteLine(messageFull); 
+                        
+                Debug.WriteLine("=== Логер ===");
+                Debug.WriteLine(messageFull);
+                Debug.WriteLine("=== === === ===");
+            }
+        }
+    }
+
+}
